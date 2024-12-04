20776d928b062e409ea124cd35050f761523adee
dzlk <dlb.diesel@gmail.com>
2024-12-04 19:44:02 +0500

    Day 4

--- days/day4/solution.py
+++ days/day4/solution.py
@@ -1,48 +1,184 @@
 import re
 from collections import Counter
+from dataclasses import dataclass
 from functools import reduce
-from typing import List
+from itertools import count
+from time import sleep
+from typing import List, Tuple
 
 import click
+from random import randint
+
+from PIL.ImageChops import screen
+from asciimatics.screen import Screen
 
 from utils.io import readfile
 
 
 @click.command()
 @click.argument('pzl', type=click.Path())
-def run_day3(pzl):
+def run_day4(pzl):
     input = readfile(pzl)
     print("Result part one: " + str(solve_part_one(input)) + "\n")
     print("Result part two: " + str(solve_part_two(input)) + "\n")
 
 
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
+
+@dataclass
+class Board:
+    words: List[List[str]]
+    width: int
+    height: int
+
+    def check_point(self, point):
+        return 0 <= point.x < self.width and 0 <= point.y < self.height
+
+    def word(self, x, y) -> str:
+        return self.words[y][x]
+
+    def word_p(self, p: Point) -> str:
+        return self.word(p.x, p.y) if self.check_point(p) else ''
+
+    def iterate(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                yield x, y
+
+
+def create_data_from_input(input: List[str]) -> Board:
+    board = Board(words=[], width=0, height=0)
+
+    for line in input:
+        board.words.append(list(line))
+
+    board.width = len(board.words[0])
+    board.height = len(board.words)
+    return board
+
+
+@dataclass
+class State:
+    board: Board
+    xy: set[Point]
+
+    count: int
+
+    def found(self):
+        self.count += 1
+
+
 def solve_part_one(input: list[str]) -> int:
-    return reduce(lambda res, expr: res + execute_expr(expr), input, 0)
+    state = State(
+        board=create_data_from_input(input),
+        xy=set(),
+        count=0,
+    )
 
+    directions = [
+        (1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)
+    ]
 
-def execute_expr(expr: str) -> int:
-    matches = re.findall(r'mul\((\d+),(\d+)\)', expr)
-    print(expr)
-    print(matches, len(matches))
+    def check_xmas(p: Point, sign_x, sign_y) -> bool:
+        board = state.board
 
-    result = 0
-    for x, y in matches:
-        result += int(x) * int(y)
+        p1 = Point(p.x + 1 * sign_x, p.y + 1 * sign_y)
+        p2 = Point(p.x + 2 * sign_x, p.y + 2 * sign_y)
+        p3 = Point(p.x + 3 * sign_x, p.y + 3 * sign_y)
 
-    return result
+        if (board.word_p(p1) == 'M' and
+                board.word_p(p2) == 'A' and
+                board.word_p(p3) == 'S'):
+            state.xy.update([p, p1, p2, p3])
+
+            return True
+        return False
+
+    def create_screen(screen):
+        for x, y in state.board.iterate():
+            p = Point(x, y)
+
+            if state.board.word_p(p) == 'X':
+                for d in directions:
+                    if check_xmas(p, d[0], d[1]):
+                        state.found()
+
+            # print_data(screen, state, p)
+
+            ev = screen.get_key()
+            if ev in (ord('Q'), ord('q')):
+                return
+
+        # sleep(1000)
+
+    Screen.wrapper(create_screen)
+
+    return state.count
 
 
 def solve_part_two(input: list[str]):
-    expr = reduce(lambda res, line: res + line, input)
-    return execute_expr_extra(expr)
+    state = State(
+        board=create_data_from_input(input),
+        xy=set(),
+        count=0,
+    )
+
+    def check_xmas(p: Point) -> bool:
+        board = state.board
+
+        p1 = Point(p.x - 1, p.y - 1)
+        p2 = Point(p.x + 1, p.y + 1)
+        w12 = board.word_p(p1) + board.word_p(p2)
+
+        p3 = Point(p.x + 1, p.y - 1)
+        p4 = Point(p.x - 1, p.y + 1)
+        w34 = board.word_p(p3) + board.word_p(p4)
+
+        if (w12 == 'MS' or w12 == 'SM') and (w34 == 'MS' or w34 == 'SM'):
+            state.xy.update([p, p1, p2, p3, p4])
+            return True
+
+        return False
+
+    def create_screen(screen):
+        for x, y in state.board.iterate():
+            p = Point(x, y)
+
+            if state.board.word_p(p) == 'A':
+                if check_xmas(p):
+                    state.found()
+
+            # print_data(screen, state, p)
+
+            ev = screen.get_key()
+            if ev in (ord('Q'), ord('q')):
+                return
+
+        # sleep(1000)
+
+    Screen.wrapper(create_screen)
+
+    return state.count
 
 
-def execute_expr_extra(expr: str) -> int:
-    parts = expr.split("don't()")
+def print_data(screen, state, pos: Point):
+    for x, y in state.board.iterate():
+        point = Point(x, y)
+        color = 7
+        if point == pos:
+            color = 6
+        elif point in state.xy:
+            color = 4
 
-    result = execute_expr(parts[0])
-    for part in parts[1:]:
-        result = reduce(lambda res, expr: res + execute_expr(expr),
-                        part.split('do()')[1:],
-                        result)
-    return result
+        screen.print_at(state.board.word(x, y), x, y + 2, colour=color)
+
+        # if x == 0:
+        #     screen.refresh()
+        # screen.refresh()
+
+    screen.print_at(f"Count: {state.count}", 0, 0, colour=7)
+    screen.refresh()
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import days.day1.solution as day1
 import days.day2.solution as day2
 import days.day3.solution as day3
+import days.day4.solution as day4
 
 
 def init():
     # day1.run_day1()
     # day2.run_day2()
-    day3.run_day3()
+    # day3.run_day3()
+    day4.run_day4()
 
 
 if __name__ == '__main__':
